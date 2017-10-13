132f82c3b6cd6778556a18bd93bf56514558afd7
Erathriel <geoffreynoro@orange.fr>
2017-10-13 10:07:22 +0200

    tp3 amelioration pour tout type de matrice

--- systeme/tp3/tp3_ex1.c
+++ systeme/tp3/tp3_ex1.c
@@ -21,7 +21,8 @@ typedef struct
 {
 	int min;
 	int max;
-	int taille;
+	int ligne;
+	int colonne;
 	int nLigne;
 	int somme;
 	int **matrix;
@@ -29,21 +30,26 @@ typedef struct
 } arguments;
 
 void *thread(void *arg){
+	// represente la somme de la ligne
+	int s=0;
 	// on recupere les arguments donne au thread
 	arguments *args = (arguments *) arg;
 	pthread_mutex_lock(&mutex); // verrouillage du mutex
 	/* on cree un tableau d'entier correspondant a la ligne de la matrice
 	du thread actif */
-	int *newligne=calloc(args->taille,sizeof(int));
+	int *newligne=calloc(args->colonne,sizeof(int));
 	// on rempli la ligne de nombre aleatoire
-	for(int j=0; j<args->taille; j++){
+	for(int j=0; j<args->colonne; j++){
 		// on cree un nombre aleatoire entre les bornes definis
 		newligne[j]=rand()%(args->max-args->min+1) + args->min;
 		printf("[%d]", newligne[j] );
-		// on l'ajoute à la somme des precedents ( initialiser a 0)
-		args->somme+=newligne[j];
+		// on l'ajoute à la somme des precedents éléments ( initialiser a 0)
+		s+=newligne[j];
 	}
 	printf("\n");
+	/* on ajoute la somme de la ligne du thread acturel a la somme total 
+	des elements de la matrice */
+	args->somme+=s;
 	pthread_mutex_unlock(&mutex); // deverrouillage du mutex
 	pthread_exit(newligne); // on renvoie la ligne que l'on a cree
 }
@@ -66,14 +72,14 @@ int createThread(pthread_t *t, arguments *args){
 int main(int argc, char *argv[])
 {
 	// test si le nombre d'argument est inferieur a 4
-	if(argc<4)
+	if(argc<5)
 	{
-		printf("Veuillez saisir le bon nombre d'argument de la maniere suivante : \n ./nomDeLExecutable borneMin borneMax tailleMatrice \n");
+		printf("Veuillez saisir le bon nombre d'argument de la maniere suivante : \n ./nomDeLExecutable borneMin borneMax nbLigne nbColonne \n");
 		return EXIT_FAILURE;
 	}
 	// test si le nombre d'argument est superieur a 4
-	else if(argc>4) {
-		printf("Nombre d'argument trop grand veuillez utiliser la syntaxe suivante : \n ./nomDeLExecutable borneMin borneMax tailleMatrice \n");
+	else if(argc>5) {
+		printf("Nombre d'argument trop grand veuillez utiliser la syntaxe suivante : \n ./nomDeLExecutable borneMin borneMax nbLigne nbColonne \n");
 		return EXIT_FAILURE;
 	}
 	else {
@@ -83,29 +89,31 @@ int main(int argc, char *argv[])
 		int arg1=atoi(argv[1]);
 		int arg2=atoi(argv[2]);
 		int arg3=atoi(argv[3]);
+		int arg4=atoi(argv[4]);
 		/* creation d'une variable de type struct arguments et 
 		instanciation des elements qui la composent et allocation de 
 		l'espace memoire pour le tableau composant la matrice */
 		arguments args;
 		args.min=arg1;
 		args.max=arg2;
-		args.taille=arg3;
+		args.ligne=arg3;
+		args.colonne=arg4;
 		args.somme=0;
-		args.matrix=calloc(args.taille,sizeof(args.taille));
-		for(int i=0; i<args.taille; i++){
-			args.matrix[i]=calloc(args.taille,sizeof(args.taille));
+		args.matrix=calloc(args.ligne,sizeof(args.ligne));
+		for(int i=0; i<args.ligne; i++){
+			args.matrix[i]=calloc(args.colonne,sizeof(args.colonne));
 		}
 		// On declare un tableau de 4 threads
-		pthread_t thread[args.taille];
+		pthread_t thread[args.ligne];
 		// initialisation de srand
 		srand(time(NULL));
 		// creation des threads
-		for(int k=0; k<args.taille; k++){
+		for(int k=0; k<args.ligne; k++){
 			createThread(&thread[k], &args);
 		}
 		/* permet de creer une ligne par threads en attendant la fin
 		du thread precedent */
-		for(int l=0; l<args.taille; l++){
+		for(int l=0; l<args.ligne; l++){
 			int *ligne;
 			if (pthread_join(thread[l], (void**) &ligne)){
 				perror("pthread_join");
@@ -113,7 +121,8 @@ int main(int argc, char *argv[])
 			}
 		}
 		// affichage de la somme
-		printf("La somme de cette matrice carre de taille 4 est de %d\n", args.somme);	
+		printf("La somme de cette matrice carre de taille 4 est de %d\n", args.somme);
+		pthread_mutex_destroy(&mutex);	
 
 		return 0;
 	}
