2fae85fdf2bba7e56368ff5f1e27ab6ea77b4409
Erathriel <geoffreynoro@orange.fr>
2017-10-20 10:17:20 +0200

    Amelioration de break_all

--- security/challenge/break_all.c
+++ security/challenge/break_all.c
@@ -7,31 +7,41 @@
 #define ALPHABET_SIZE 26
 
 void Caesar_Break(){
-	FILE* file=NULL;
+	FILE* fileCipherText=NULL;
+	FILE* filePlainText=NULL;
 	char *fileName;
-	char charAt;
+	char *fileName2;
 	int *alphabetFreq;
+
+	char charAt;
 	char cryptedLetter=0;
     int freqCryptedLetter=0;
     int key=0;
+
 	alphabetFreq=calloc(ALPHABET_SIZE,sizeof(int));
 	fileName=malloc(sizeof(char)*256);
-	printf("choisir le message a decrypter :\n");
+	fileName2=malloc(sizeof(char)*256);
+	
+	printf("Saisir le nom du fichier .txt contenant le message chiffré :\n");
 	scanf("%s",fileName);
-	file = fopen(fileName, "r");
-	if (file != NULL)
+	printf("Saisir le nom du fichier .txt qui recevras le message décrypté :\n");
+	scanf("%s",fileName2);
+	fileCipherText = fopen(fileName, "r");
+	filePlainText = fopen(fileName2, "w");
+	
+
+
+	if (fileCipherText != NULL && filePlainText != NULL)
 	{
 		do{
-        	charAt=fgetc(file);
-        	//printf("%c\n", charAt);
+        	charAt=fgetc(fileCipherText);
         	if (charAt >= 'A' && charAt <= 'Z')
         	{
-        		//printf("%d\n", charAt-'A');
         		alphabetFreq[charAt-'A'] += 1;
         	}
         }
         while(charAt != EOF);
-        fclose(file);
+        fclose(fileCipherText);
         for (int i = 0; i < ALPHABET_SIZE; ++i)
         {
         	if(alphabetFreq[i] > freqCryptedLetter){
@@ -41,17 +51,53 @@ void Caesar_Break(){
         	unsigned char c = (unsigned char) (i+65);
         	printf("La lettre %c apparait %d fois dans le texte.\n", c ,alphabetFreq[i]);
         }
-        printf("La lettre la plus utilisée dans ce text est : %c\n", cryptedLetter);
 
+        printf("La lettre la plus utilisée dans ce text est : %c\n", cryptedLetter);
         key = cryptedLetter - 'E';
+        if (key<0)
+        {
+        	key = -key;
+        }
+        printf("La clé est donc : %d\n", key);
+        fileCipherText = fopen(fileName, "r");
+        if (fileCipherText !=NULL)
+        {
+        	do{
+        		charAt=fgetc(fileCipherText);
+        		if (charAt >= 'A' && charAt <= 'Z')
+        		{
+        			charAt = charAt - key;
+        			if (charAt < 'A')
+        			{	
+        				charAt += 26;
+        			}
+        			fputc(charAt, filePlainText);
+        		}
+        		else if(charAt == -1){
+        			fputc(' ', filePlainText);
+        		}
+        		else {
+        			fputc(charAt, filePlainText);
+        		}
+        	}
+        	while(charAt != EOF);
+        	fclose(fileCipherText);
+        	fclose(filePlainText);
+        }
+        else{
+        	printf("Impossible de lire le fichier %s !\n", fileName);
+        }
+
+        free(alphabetFreq);
 	}
+
 	else {
-		printf("Impossible de lire le fichier !\n");
+		printf("Impossible de lire le fichier %s ou %s !\n", fileName, fileName2);
 	}
 	
 }
 
-// choisir comment on veut casser le texte chiffrer
+// choisir comment on veut casser le texte chiffré
 
 void FaireChoix(){
 	int choix = 0;
@@ -60,6 +106,7 @@ void FaireChoix(){
 	printf("1) Caesar Break\n");
 	printf("2) Vigenere Break\n");
 	printf("3) Subtitution Break\n");
+	printf("Choisissez comment vous voulez casser votre message chiffré : ");
 	scanf ("%d", &choix);
 
 	switch(choix){
