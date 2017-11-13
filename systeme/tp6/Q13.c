1a7945bd7d5e1d092ea26ceb313ad97030385793
Erathriel <geoffreynoro@orange.fr>
2017-11-13 14:14:11 +0100

    system FINI A RENDRE !!!

--- systeme/tp6/Q13.c
+++ systeme/tp6/Q13.c
@@ -6,6 +6,7 @@ Auteur : NORO Geoffrey & PEGEOT Antoine
 
 ***************************************************/
 
+#define _GNU_SOURCE
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,28 +15,46 @@ Auteur : NORO Geoffrey & PEGEOT Antoine
 
 int main(int argc, char const *argv[])
 {
-	
-	int tmp=0;
-	int cpuActive = sched_getcpu();
-	cpu_set_t setCPU;
-	CPU_ZERO(&setCPU);
-	CPU_SET(0,&setCPU);
-	if (sched_setaffinity(getpid(), sizeof(setCPU), &setCPU) == -1)
+	if (argc > 1 && argc < 3)
 	{
-		perror("affinity error");
-	}
+		if (atoi(argv[1]) > -1 && atoi(argv[1]) < 8)
+		{
+			int CPUchoisi = atoi(argv[1]);
+			long int nbCPU = sysconf(_SC_NPROCESSORS_ONLN);
+			int tmp=0;
+			int cpuActive = sched_getcpu();
+			cpu_set_t setCPU;
+			CPU_ZERO(&setCPU);
+			CPU_SET(CPUchoisi,&setCPU);
 
-	
+			if (sched_setaffinity(getpid(), sizeof(setCPU), &setCPU) == -1)
+			{
+				printf("affinity error\n");
+			}
 
-	while(1){
-		tmp = sched_getcpu();
-		if(cpuActive == tmp){
-			printf("Le cpu est toujours : %d\n",tmp );
+			else{
+
+				while(1){
+
+					tmp = sched_getcpu();
+
+					if(cpuActive == tmp){
+						printf("Le cpu est toujours : %d\n",tmp );
+					}
+
+					else{
+						printf("Le cpu est devenu : %d \n",tmp );
+						cpuActive=tmp;
+					}
+				}
+			}
 		}
 		else{
-			printf("Le cpu est devenu : %d \n",tmp );
-			cpuActive=tmp;
+			printf("Veuillez entrer une valeur correcte !\n");
 		}
+	}
+	else{
+		printf("nombre d'arguments incorrect\n");
 	}
 
 	return 0;
