63446d77e3f075223394de60972873796becee37
Erathriel <geoffreynoro@orange.fr>
2017-11-30 12:22:38 +0100

    Serveur SR

--- systeme/tp7/serveur.c
+++ systeme/tp7/serveur.c
@@ -27,7 +27,54 @@ typedef struct sockaddr SOCKADDR;
 
 int main(int argc, char const *argv[])
 {
+	int erreur = 0;
+    SOCKET sock;
+    SOCKADDR_IN sin;
+    SOCKET csock;
+    SOCKADDR_IN csin;
+    char buffer[1024]="Hello world";
+    int port=atoi(argv[1]);
+    int sinSize = sizeof csin;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sock == INVALID_SOCKET)
+	{
+    	printf("socket()\n");
+    	return EXIT_FAILURE;
+	}
+
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(port);
+    bzero(sin.sin_zero,8);
 	
-	
-	return 0;
+    if(bind (sock, (SOCKADDR *) &sin, sizeof sin) == SOCKET_ERROR)
+	{
+    	printf("bind()\n");
+        return EXIT_FAILURE;
+	}
+
+	if(listen(sock, 5) == SOCKET_ERROR)
+	{
+	    printf("listen()\n");
+	    return EXIT_FAILURE;
+	}
+
+	csock = accept(sock, (SOCKADDR *)&csin, &sinSize);
+
+	if (csock == INVALID_SOCKET)
+	{
+		printf("accept()\n");
+		return EXIT_FAILURE;
+	}
+
+	send(csock, &buffer, sizeof(buffer),0);
+
+    shutdown(csock,SHUT_RDWR);
+    close(csock);
+	shutdown(sock,SHUT_RDWR);
+    close(sock);
+
+	return EXIT_SUCCESS;
 }
