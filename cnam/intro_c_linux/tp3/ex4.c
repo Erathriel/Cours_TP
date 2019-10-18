4af7ed874c4feac4b682b81b077e149ff89c6c79
Erathriel <geoffreynoro@orange.fr>
2019-10-18 12:22:19 +0200

    sss

--- cnam/intro_c_linux/tp3/ex4.c
+++ cnam/intro_c_linux/tp3/ex4.c
@@ -23,6 +23,23 @@ int main(int argc, char const *argv[])
 		str1 = argv[1];
 		str2 = argv[2];
 
+		if (sizeof(str1) == sizeof(str2))
+		{
+			for (int i = 0; i < sizeof(str1); ++i)
+			{
+				if (str1[i] != str2[i])
+				{
+					printf("char diff\n");
+				}
+				else{
+					printf("chaine identique\n");
+				}
+			}
+		}
+		else{
+			printf("Tailles différentes\n");
+		}
+
 		printf("%s\n", str1);
 		printf("%s\n", str2);
 
