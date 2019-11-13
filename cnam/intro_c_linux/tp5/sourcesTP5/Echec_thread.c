07f8d146097339d4161f95dc6f48498385204ec2
Erathriel <geoffreynoro@orange.fr>
2019-11-13 12:18:38 +0100

    Makefile ameliore

--- cnam/intro_c_linux/tp5/sourcesTP5/Echec_thread.c
+++ cnam/intro_c_linux/tp5/sourcesTP5/Echec_thread.c
@@ -11,15 +11,30 @@ int main(void) {
 	pthread_t thr;
 	int nbre = 0;
 	while ( pthread_create(&thr,NULL,fonction_thread,NULL) == 0) {
+		printf("Thread %d créer\n", nbre);
 		nbre ++ ;
+		pthread_join(thr,NULL);
+		usleep(10000);
 	}
 
-	fprintf(stderr,"Echec de creation après %d \n",
-		nbre);
+	fprintf(stderr,"Echec de creation après %d \n", nbre);
 	return EXIT_SUCCESS;
 }
 			
 void *fonction_thread (void * arg) {
+	printf("Hello world !\n");
+	#ifdef __LINUX
+		char *os = "A true OS";
+		printf("%s\n", os);
+		getchar();
+		system("clear");
+	#endif
+	#ifdef __WINDOWS
+		char *os = "A blue screen";
+		printf("%s\n", os);
+		system("PAUSE");
+		system("CLS");
+	#endif
 	return NULL ;
 
 }  
