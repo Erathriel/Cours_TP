46b3985996d1eb020177fc9b38d65c94057e989f
Erathriel <geoffreynoro@orange.fr>
2019-11-15 17:22:20 +0100

    prg1.c avec 2 tubes

--- cnam/linux_et_prog/prg1.c
+++ cnam/linux_et_prog/prg1.c
@@ -1,6 +1,6 @@
 /****************************************************
 |													|
-|			Fichier : prg1.c						|
+|			Fichier : prg4.c						|
 |			Auteur : NORO Geoffrey					|
 |													|
 ****************************************************/
@@ -21,12 +21,17 @@ void erreur(const char *msg)
 int main(void)
 {
     pid_t pid;
-    int tube[2];
+    int tube[2], tube2[2];
     int ret_out, ret_in;
     char *buffer;
 
     if (pipe(tube) == -1) {
-	erreur("Erreur de creation du pipe");
+	erreur("Erreur de creation du pipe tube");
+	exit(EXIT_FAILURE);
+    }
+
+    if (pipe(tube2) == -1) {
+	erreur("Erreur de creation du pipe tube2");
 	exit(EXIT_FAILURE);
     }
 
@@ -34,57 +39,48 @@ int main(void)
     buffer = (char *) malloc(NBCAR * sizeof(char));
 
 
-    switch (pid = fork()) {
-
-    case (pid_t) - 1:
-	erreur("fork");
-
-    case (pid_t) 0:		/* Fils */
+    switch (pid = fork()) 
 	{
-#if 0
-	    close(tube[1]);	/* Fermeture en ecriture */
-#endif
+	case (pid_t) - 1:
+		erreur("fork");
 
-	    if (printf
-		("Je suis le fils de PID %d\n Lecture dans le tube : %s\n Nbr Caracteres lus: %d\n",
-		 getpid(), buffer, ret_in =
-		 read(tube[0], buffer, NBCAR - 1)) == -1) {
-		erreur(" Pb Lecture ");
+	case (pid_t) 0: /* Fils */
+		close(tube[1]);	/* Fermeture en ecriture */
+		close(tube2[0]);
+		if (printf("Je suis le fils de PID %d\n Lecture dans le tube : %s\n Nbr Caracteres lus: %d\n",getpid(), buffer, ret_in =read(tube[0], buffer, NBCAR - 1)) == -1) 
+		{
+			erreur(" Pb Lecture ");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(buffer, "Je passe dans le tube2");
+		fprintf(stdout, "Je suis le fils de PID %d\n Ecriture dans le Tube : %s\n Taille du buffer %d\n",getpid(), buffer, NBCAR);
+		fprintf(stdout, "\nNombre de caractere ecrit dans le Tube %d\n", ret_out = write(tube2[1], buffer, NBCAR));
+		write(tube2[1],buffer,NBCAR);
+		close(tube[0]);
+		close(tube2[1]);
+		exit(0);
+		break;
+	default:			/* Pere */
+		close(tube[0]);		/* Fermeture en lecture */
+		close(tube2[1]);
+		if(fprintf("Je suis le père de PID %d\n Lecture dans le tube : %s\n Nbr Caracteres lus: %d\n", getpid(), buffer, ret_in=read(tube2[0], buffer, NBCAR - 1)==-1))
+		{
+ erreur("Pb lecture père");
+exit(EXIT_FAILURE);
+ };
+		strcpy(buffer, "Je passe dans le tube1");
+		fprintf(stdout,"Je suis le Pere de PID %d\n Ecriture dans le Tube : %s\n Taille du buffer %d\n",getpid(), buffer, NBCAR);
+		if (fprintf(stdout, "\tNombre de caractere ecrit dans le Tube %d\n",ret_out = write(tube[1], buffer, NBCAR)) == -1) 
+		{
+		erreur(" Pb Ecriture ");
 		exit(EXIT_FAILURE);
-	    }
-#if 0
-	    close(tube[0]);
-#endif
+		}
+		close(tube[1]);
+		close(tube2[0]);
+		wait(NULL);
+		break;
 
-	    exit(0);
-	    break;
 	}
-
-    default:			/* Pere */
-
-#if 0
-	close(tube[0]);		/* Fermeture en lecture */
-#endif
-
-	strcpy(buffer, "Je passe dans le tube");
-	fprintf(stdout,
-		"Je suis le Pere de PID %d\n Ecriture dans le Tube : %s\n Taille du buffer %d\n",
-		getpid(), buffer, NBCAR);
-	if (fprintf
-	    (stdout, "Nombre de caractere ecrit dans le Tube %d\n",
-	     ret_out = write(tube[1], buffer, NBCAR)) == -1) {
-	    erreur(" Pb Ecriture ");
-	    exit(EXIT_FAILURE);
-	}
-#if 0
-	close(tube[1]);
-#endif
-
-	wait(NULL);
-	break;
-
-    }
-
     free(buffer);
     return EXIT_SUCCESS;
 }
