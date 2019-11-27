29e33c73cab10475476e340978646119c8a19fef
Erathriel <geoffreynoro@orange.fr>
2019-11-27 12:03:38 +0100

    code mult bug et non bug

--- cnam/intro_c_linux/tp5/sourcesTP5/mult.c
+++ cnam/intro_c_linux/tp5/sourcesTP5/mult.c
@@ -8,59 +8,77 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "mult.h"
 
-void *thread(void *arg){
-	int *arguments = (int *) arg;
-	int resultat;
-	for (int i = 0; i < 3; ++i)
-	{
-		printf("%d \n", arguments[i]);
-	}
-	if (arguments[0] != 0)
-	{
-		resultat = arguments[1]*arguments[2];
-		printf("Le resultat de %d + %d est : %d \n", arguments[1], arguments[2], resultat);
-	}
-
-	pthread_exit(NULL);
+int multi(int a, int b){
+	return a*b;
 }
 
-int threadCreator(pthread *t, int *i){
-	int ret = pthread_create(t, NULL, thread, (void *) i);
+void *thread(void *d) {
+	data *da = malloc(sizeof(data));
+	da = (data *) d;
+	printf("%d\n", da->num1);
+	printf("%d\n", da->num2);
+	int r = multi(da->num1,da->num2);
+	printf("%d\n", r);
+	pthread_exit((void *)r);
+	free(da);
+}
+
+
+int createThread(pthread_t *t, data *d){
+	int ret = pthread_create(t,NULL,thread, (void *) d);
 	if (ret == -1)
 	{
 		perror("pthread_create error");
+		return EXIT_FAILURE;
 	}
 	return ret;
 }
 
 int main(int argc, char const *argv[])
 {
-	pthread_t pthid[3];
-	int a,b,c,d;
-	int arg[3];
+	data *d = malloc(sizeof(data));
+	data *d2 = malloc(sizeof(data));
+	pthread_t pthid[2];
+	int res;
 
-	for (int i = 0; i < 3; ++i)
+	for (int i=0; i < 2; ++i)
 	{
-		switch(i)
-		{
+		switch(i){
+			case 0:
+				d->num1=1;
+				d->num2=2;
+				createThread(&pthid[i], d);
+				break;
 			case 1:
-				arg[0] = i;
-				arg[1] = a;
-				arg[2] = b;
-			case 2:
-				arg[0] = i;
-				arg[1] = c;
-				arg[2] = d;
-			case 3:
-				arg[0] = i;
-				arg[1] = 0;
-				arg[2] = 0;
+				d2->num1=3;
+				d2->num2=4;
+				createThread(&pthid[i], d2);
+				break;
 			default:
-				printf("Erreur\n");
+				printf("ERROR\n");
+				break;
 		}
-		threadCreator(&pthid[i], arg);
+		// bug
+		/*if (pthread_join(pthid[i],&res))
+		{
+			perror("pthread_join");
+		}
+		printf("Le resultat de la multiplication est : %d\n",res);
+		sleep(1);*/
+	}
+	// code qui marche
+	for (int i = 0; i < 2; ++i)
+	{
+		if (pthread_join(pthid[i],&res))
+		{
+			perror("pthread_join");
+		}
+		printf("Le resultat de la multiplication est : %d\n",res);
 	}
 
+	free(d);
+	free(d2);
 	return 0;
 }
---
+++ cnam/intro_c_linux/tp5/sourcesTP5/mult.h
@@ -0,0 +1,11 @@
+#ifndef MULT_H
+#define MULT_H
+
+typedef struct
+{
+	int num1;
+	int num2;
+} data;
+
+
+#endif // MULT_H
