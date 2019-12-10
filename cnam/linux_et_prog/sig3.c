df4519a0c76f82690779a40ddbc3da0266ea06d4
Erathriel <geoffreynoro@orange.fr>
2019-12-10 15:18:03 +0100

    modif

--- cnam/linux_et_prog/sig3.c
+++ cnam/linux_et_prog/sig3.c
@@ -15,7 +15,7 @@
 void message1(int n){
 	printf("Message 1 : Signal %d : recu\n", n);
 	//sleep(2);
-	//kill(getppid(),SIGUSR2);
+	kill(getppid(),SIGUSR2);
 }
 void message2(int n){printf("Message 2 : Signal %d : recu\n", n);}
 
@@ -34,8 +34,8 @@ int main(int argc, char const *argv[])
 			fprintf(stderr,"Error %d in fork: %s\n", errno,sys_errlist[errno]);
 			exit(errno);
 		case 0: // Dans le fils
-			sleep(2);
 			printf("Je suis dans le fils\n");
+			sleep(2);
 			kill(getppid(),SIGUSR2);
 			exit(0);
 			break;
